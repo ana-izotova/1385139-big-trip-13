919a0d152f7e80179f4c44be65d559cefec4eb86
Anastasia <ana.izotova@gmail.com>
2020-11-24 18:01:23 +0300

    edit tripInfoTemplate to accept mock data

--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -1,13 +1,30 @@
-const createTripInfoTemplate = () => {
+import {destinations} from "../mock/event-cards.js";
+
+const createTripInfoTemplate = (cards) => {
+  const tripRoute = destinations.length > 3 ?
+    `${destinations[0]} — ... — ${destinations[destinations.length - 1]}` :
+    `${destinations[0]} — ${destinations[1]} — ${destinations[2]}`;
+
+  const tripStartDate = cards[0].startDate.format(`MMM D`);
+  const tripEndDate = cards[cards.length - 1].endDate.format(`MMM D`);
+
+  const tripCost = cards.reduce(((cardsAcc, card) => {
+    const selectedOffersTotalPrice = card.offers
+      .filter((offer) => offer.checked)
+      .reduce(((offersAcc, offer) => offersAcc + offer.price), 0);
+    return selectedOffersTotalPrice + cardsAcc + card.price;
+  }), 0);
+
+
   return `<section class="trip-main__trip-info  trip-info">
             <div class="trip-info__main">
-              <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+              <h1 class="trip-info__title">${tripRoute}</h1>
 
-              <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+              <p class="trip-info__dates">${tripStartDate} — ${tripEndDate}</p>
             </div>
 
             <p class="trip-info__cost">
-              Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+              Total: &euro;&nbsp;<span class="trip-info__cost-value">${tripCost}</span>
             </p>
           </section>`;
 };
