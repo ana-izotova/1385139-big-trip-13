a9dd51ae67aa9167cb27ffc4dd8a6704c72608e2
Anastasia <ana.izotova@gmail.com>
2020-12-09 18:56:34 +0300

    add tripInfo presenter

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import TripRoutePresenter from "./presenter/tripRoute.js";
+import TripRoutePresenter from "./presenter/trip-route.js";
+import TripInfoPresenter from "./presenter/trip-info.js";
 import {cards} from "./mock/event-cards.js";
 
 const pageHeader = document.querySelector(`.page-header`);
 const tripInfoMainContainer = pageHeader.querySelector(`.trip-main`);
-const tripControlsContainer = tripInfoMainContainer.querySelector(`.trip-main__trip-controls`);
 
 const pageMain = document.querySelector(`.page-body__page-main`);
 const tripEventsContainer = pageMain.querySelector(`.trip-events`);
 
-const tripRoutePresenter = new TripRoutePresenter(tripEventsContainer, tripInfoMainContainer, tripControlsContainer);
+const tripRoutePresenter = new TripRoutePresenter(tripEventsContainer);
+const tripInfoPresenter = new TripInfoPresenter(tripInfoMainContainer);
+
 tripRoutePresenter.init(cards);
+tripInfoPresenter.init(cards);
--- src/presenter/trip-route.js
+++ src/presenter/trip-route.js
@@ -1,26 +1,19 @@
 import SortingView from "../view/sorting.js";
 import TripListView from "../view/trip-list.js";
 import EmptyTripListView from "../view/trip-list-empty.js";
-import TripInfoView from "../view/trip-info";
-import MenuView from "../view/menu.js";
-import FiltersView from "../view/filters.js";
-import TripPointPresenter from "./tripPoint.js";
+import TripPointPresenter from "./trip-point.js";
 import {render, RenderPosition} from "../utils/render.js";
 import {updateItem} from "../utils/common.js";
 
 class TripRoute {
-  constructor(tripEventsContainer, tripInfoMainContainer, tripControlsContainer) {
+  constructor(tripEventsContainer) {
     this._tripEventsContainer = tripEventsContainer;
-    this._tripInfoMainContainer = tripInfoMainContainer;
-    this._tripControlsContainer = tripControlsContainer;
 
     this._tripPresenter = {};
 
     this._tripListComponent = new TripListView();
     this._sortingComponent = new SortingView();
     this._emptyTripListComponent = new EmptyTripListView();
-    this._menuComponent = new MenuView();
-    this._filtersComponent = new FiltersView();
 
     this._handleTripChange = this._handleTripChange.bind(this);
     this._handleModeChange = this._handleModeChange.bind(this);
@@ -49,19 +42,6 @@ class TripRoute {
     render(this._tripEventsContainer, this._tripListComponent, RenderPosition.BEFOREEND);
   }
 
-  _renderTripInfo() {
-    this._tripInfoComponent = new TripInfoView(this._tripCards);
-    render(this._tripInfoMainContainer, this._tripInfoComponent, RenderPosition.AFTERBEGIN);
-  }
-
-  _renderTripMenu() {
-    render(this._tripControlsContainer, this._menuComponent, RenderPosition.AFTERBEGIN);
-  }
-
-  _renderTripFilters() {
-    render(this._tripControlsContainer, this._filtersComponent, RenderPosition.BEFOREEND);
-  }
-
   _renderEmptyTripList() {
     render(this._tripEventsContainer, this._emptyTripListComponent, RenderPosition.BEFOREEND);
   }
@@ -85,14 +65,11 @@ class TripRoute {
   }
 
   _renderTripRoute() {
-    this._renderTripMenu();
-    this._renderTripFilters();
     this._renderSort();
     this._renderTripList();
     if (this._tripCards.length === 0) {
       this._renderEmptyTripList();
     } else {
-      this._renderTripInfo();
       this._renderTripPoints();
     }
   }
