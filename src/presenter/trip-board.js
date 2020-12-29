0db4e981234f94a89070e5f737b4dbf890bd0568
Anastasia <ana.izotova@gmail.com>
2020-12-29 19:09:56 +0300

    modify databinding

--- src/presenter/trip-board.js
+++ src/presenter/trip-board.js
@@ -4,7 +4,7 @@ import EmptyTripListView from "../view/trip-list-empty.js";
 import TripPointPresenter from "./point.js";
 import {render, RenderPosition} from "../utils/render.js";
 import {sortTripCardsByPrice, sortTripCardsByDuration} from "../utils/trip.js";
-import {SortType} from "../const.js";
+import {SortType, UpdateType, UserAction} from "../const.js";
 
 class TripBoard {
   constructor(pointsContainer, pointsModel) {
@@ -80,19 +80,31 @@ class TripBoard {
   }
 
   _handleViewAction(actionType, updateType, update) {
-    console.log(actionType, updateType, update);
-    // Здесь будем вызывать обновление модели.
-    // actionType - действие пользователя, нужно чтобы понять, какой метод модели вызвать
-    // updateType - тип изменений, нужно чтобы понять, что после нужно обновить
-    // update - обновленные данные
+    switch (actionType) {
+      case UserAction.UPDATE_POINT:
+        this._pointsModel.updatePoint(updateType, update);
+        break;
+      case UserAction.ADD_POINT:
+        this._pointsModel.addPoint(updateType, update);
+        break;
+      case UserAction.DELETE_POINT:
+        this._pointsModel.deletePoint(updateType, update);
+        break;
+    }
   }
 
   _handleModelEvent(updateType, data) {
-    console.log(updateType, data);
-    // В зависимости от типа изменений решаем, что делать:
-    // - обновить часть списка (например, когда поменялось описание)
-    // - обновить список (например, когда задача ушла в архив)
-    // - обновить всю доску (например, при переключении фильтра)
+    switch (updateType) {
+      case UpdateType.PATCH:
+        this._tripPresenter[data.id].init(data);
+        break;
+      case UpdateType.MINOR:
+        //
+        break;
+      case UpdateType.MAJOR:
+        //
+        break;
+    }
   }
 
   _handleModeChange() {
